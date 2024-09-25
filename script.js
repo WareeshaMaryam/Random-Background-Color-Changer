36a682026babbfd2ba292c60d9f83bee177e3bdf
WareeshaMaryam <wareeshamaryam8@gmail.com>
2024-09-25 12:03:24 +0500

    done

---
+++ script.js
@@ -0,0 +1,7 @@
+function color1(){
+document.getElementById("color1").style.backgroundColor=`rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`
+}
+
+function Color2(){
+    document.getElementById("color2").style.backgroundColor = `rgb(${Math.floor(Math.random()*225)},${Math.floor(Math.random()*225)},${Math.floor(Math.random()*225)})
+`}
